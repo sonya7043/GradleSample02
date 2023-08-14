1aa49c8d05aec6f1f7999cb3c6a2a9c44d02663d
sonya7043 <sonya7043@naver.com>
2023-08-14 11:59:29 +0900

    add MainController

--- src/main/java/com/example/demo/MemberController.java
+++ src/main/java/com/example/demo/MemberController.java
@@ -5,15 +5,25 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainController {
+@RequestMapping
+public class MemberController {
 
 	@GetMapping({"", "/"})
 	public ResponseEntity<?> index(){
 		Map<String, Object> map = new HashMap<>();
-		map.put("key", "333");
+		map.put("key", "11111");
 		return ResponseEntity.ok().body(map);
 	}
+	
+	@GetMapping({"", "/"})
+	public ResponseEntity<?> health(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("key", "2222");
+		return ResponseEntity.ok().body(map);
+	}
+	
 }
